a792f9f62d9cc83208aeece1e512a13e9b174d6a
Anshu Chaurasiya <154973282+AnshuChaurashiya@users.noreply.github.com>
2025-12-10 03:06:06 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
@@ -8,8 +7,8 @@ import Testimonial from "./components/testimonial/Testimonial";
 import Brands from "./pages/Brands";
 import CardInfo from "./pages/Card-info";
 import CardCompare from "./pages/CardCompare";
-import { BlackCard } from "./pages/Cards/BlackCard";
-import WhiteCard from "./pages/Cards/whiteCard";
+import { BlackCard } from "./pages/BlackCard";
+import WhiteCard from "./pages/whiteCard";
 import CustomerReviews from "./pages/CustomerReviews";
 import HomeContent from "./pages/HomeContent";
 import Join from "./pages/Join";
@@ -17,30 +16,7 @@ import Join from "./pages/Join";
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
-  useEffect(() => {
-    // 🔥 Init Lenis
-    const lenis = new Lenis({
-      duration: 1.4,
-      smooth: true,
-      smoothTouch: true,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    });
-
-    // 🔥 Sync Lenis with GSAP ScrollTrigger
-    lenis.on("scroll", ScrollTrigger.update);
-
-    const raf = (time) => {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    };
-
-    requestAnimationFrame(raf);
-
-    // Cleanup
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
+ 
 
   return (
     <>
